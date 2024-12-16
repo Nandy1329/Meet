3e9bebbd4418849eca568693d184e92941bbd6ab
Nicholas A <nickis1329@gmail.com>
2024-12-16 12:02:49 -0800

    updated package.json homepage

--- src/components/NumberOfEvents.js
+++ src/components/NumberOfEvents.js
@@ -1,20 +1,21 @@
 // src/components/NumberOfEvents.js
 import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 
 const NumberOfEvents = ({ setCurrentNOE, setErrorAlert }) => {
   const [number, setNumber] = useState(32);
 
   const handleChange = (event) => {
-      const value = event.target.value;
-      setNumber(value)
-      if(isNaN(value) || value <= 0) {
-          setErrorAlert('Enter a valid number');
-      } else if (value > 32) {
-          setErrorAlert('Only maximum of 32 is allowed');
-      } else {
-          setErrorAlert('');
-          setCurrentNOE(value);
-      }
+    const value = parseInt(event.target.value, 10);
+    setNumber(value);
+    if (isNaN(value) || value <= 0) {
+      setErrorAlert('Enter a valid number');
+    } else if (value > 32) {
+      setErrorAlert('Only a maximum of 32 is allowed');
+    } else {
+      setErrorAlert('');
+      setCurrentNOE(value);
+    }
   };
 
   return (
@@ -31,6 +32,11 @@ const NumberOfEvents = ({ setCurrentNOE, setErrorAlert }) => {
       />
     </div>
   );
+};
+
+NumberOfEvents.propTypes = {
+  setCurrentNOE: PropTypes.func.isRequired,
+  setErrorAlert: PropTypes.func.isRequired,
 };
 
 export default NumberOfEvents;
