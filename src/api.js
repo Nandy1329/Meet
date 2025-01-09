f1ea6f41258faf4340a2bff8063b5c36990cba1d
Nicholas A <nickis1329@gmail.com>
2025-01-09 10:30:05 -0800

    added offline capabilities

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
-/* eslint-disable no-useless-concat */
 // src/api.js
 
 import mockData from './mock-data';
+import NProgress from 'nprogress';
 
 /**
  *
@@ -17,85 +17,9 @@ export const extractLocations = (events) => {
   return locations;
 };
 
-/**
- * Function to extract specific properties from each event
- * @param {*} events - Array of events to process
- * @returns {Array} - Array of event objects with specific properties
- */
-export const getEventDetails = (events) => {
-  return events.map((event) => ({
-    summary: event.summary,
-    start: event.created,
-    location: event.location,
-    htmlLink: event.htmlLink,
-    description: event.description,
-  }));
-};
-
-const checkToken = async (accessToken) => {
-  const response = await fetch(
-    `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
-  );
-  const result = await response.json();
-  return result;
-};
-
-/**
- *
- * This function will fetch the list of all events
- */
-export const getEvents = async () => {
-  if (window.location.href.startsWith("http://localhost")) {
-    return mockData;
-  }
-
-  const token = await getAccessToken();
-
-  if (token) {
-    removeQuery();
-    const url =  "https://muylbuwi61.execute-api.us-west-2.amazonaws.com/dev/api/get-events" + "/" + token;
-    const response = await fetch(url);
-    const result = await response.json();
-    if (result) {
-      return result.events;
-    } else return null; 
-  }
-};
-
-const removeQuery = () => {
-  let newurl;
-  if (window.history.pushState && window.location.pathname) {
-    newurl =
-      window.location.protocol +
-      "//" +
-      window.location.host +
-      window.location.pathname;
-    window.history.pushState("", "", newurl);
-  } else {
-    newurl = window.location.protocol + "//" + window.location.host;
-    window.history.pushState("", "", newurl);
-  }
-};
-
-const getToken = async (code) => {
-  try {
-    const encodeCode = encodeURIComponent(code);
- 
-    const response = await fetch( 'https://muylbuwi61.execute-api.us-west-2.amazonaws.com/dev/api/token' + '/' + encodeCode);
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`)
-    }
-    const { access_token } = await response.json();
-    access_token && localStorage.setItem("access_token", access_token);
-    return access_token;
-  } catch (error) {
-    error.json();
-  }
- }
-
 export const getAccessToken = async () => {
   const accessToken = localStorage.getItem('access_token');
-  
+
   const tokenCheck = accessToken && (await checkToken(accessToken));
 
   if (!accessToken || tokenCheck.error) {
@@ -113,4 +37,66 @@ export const getAccessToken = async () => {
     return code && getToken(code);
   }
   return accessToken;
+};
+
+const checkToken = async (accessToken) => {
+  const response = await fetch(
+    `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
+  );
+  const result = await response.json();
+  return result;
+};
+
+
+const getToken = async (code) => {
+  const encodeCode = encodeURIComponent(code);
+  const response = await fetch('https://muylbuwi61.execute-api.us-west-2.amazonaws.com/dev/api/token' + '/' + encodeCode);
+  const { access_token } = await response.json();
+  access_token && localStorage.setItem("access_token", access_token);
+
+  return access_token;
+};
+
+/**
+ *
+ * This function will fetch the list of all events
+ */
+export const getEvents = async () => {
+  if (window.location.href.startsWith('http://localhost')) {
+    return mockData;
+  }
+
+  if (!navigator.onLine) {
+    const events = localStorage.getItem("lastEvents");
+    NProgress.done();
+    return events ? JSON.parse(events) : [];
+  }
+  const token = await getAccessToken();
+
+  const removeQuery = () => {
+    let newurl;
+    if (window.history.pushState && window.location.pathname) {
+      newurl =
+        window.location.protocol +
+        "//" +
+        window.location.host +
+        window.location.pathname;
+      window.history.pushState("", "", newurl);
+    } else {
+      newurl = window.location.protocol + "//" + window.location.host;
+      window.history.pushState("", "", newurl);
+    }
+  };
+
+  if (token) {
+    removeQuery();
+    const url = `https://muylbuwi61.execute-api.us-west-2.amazonaws.com/dev/api/get-events/${token}`;
+    const response = await fetch(url);
+    const result = await response.json();
+    if (result) {
+      NProgress.done();
+      localStorage.setItem("lastEvents", JSON.stringify(result.events));
+      return result.events;
+    } else return null;
+  }
 };
