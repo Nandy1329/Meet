97415c8148e4303696031f7da80617bfe0bea49e
Nicholas A <nickis1329@gmail.com>
2025-02-06 09:28:41 -0800

    Updated package.json scripts

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -5,7 +5,7 @@ const { google } = require("googleapis");
 const calendar = google.calendar("v3");
 const SCOPES = ["https://www.googleapis.com/auth/calendar.events.public.readonly"];
 const { CLIENT_SECRET, CLIENT_ID, CALENDAR_ID } = process.env;
-const REDIRECT_URI = "https://nandy1329.github.io/Meet/";
+const redirect_uris = "https://nandy1329.github.io/Meet/";
 
 const oAuth2Client = new google.auth.OAuth2(
   CLIENT_ID,
